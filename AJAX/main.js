34c33801567f21e77be7fcc1b3ac3c6061ea710e
RAMZES15 <romactuk@gmail.com>
2023-04-22 01:09:36 +0300

    HW

---
+++ AJAX/main.js
@@ -0,0 +1,28 @@
+'use strict'
+//Я можу краще але небуло часу 
+const search = document.getElementById('search')
+const searchBtn = document.getElementById('search-btn')
+const block = document.getElementById('block')
+
+searchBtn.addEventListener('click', () => {
+    let getName = String(search.value)
+    search.value = ''
+    getName = getName.trim().split(' ').join('+')
+    let searchName = `t=${getName}&plot=full`
+    getMovie(searchName)
+})
+
+function getMovie(searchName){
+    fetch(`http://www.omdbapi.com/?apikey=5b88c3fb&${searchName}`)
+        .then(movie => movie.json())
+        .then(infoMovie => {
+            block.innerHTML = ''
+            for(let key in infoMovie){
+                block.innerHTML += `
+                <div>${key}: ${infoMovie[key]}</div>
+                `
+            }
+        })  
+}
+
+
